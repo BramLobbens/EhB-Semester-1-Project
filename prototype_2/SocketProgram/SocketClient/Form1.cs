56ee92d2a7ac2fa40ca0ddcbc14b010b3e8aa69f
BramLobbens <lobbensbram@gmail.com>
2019-12-01 01:44:05 +0100

    first commit

---
+++ prototype_2/SocketProgram/SocketClient/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SocketClient
+{
+    public partial class Form1 : Form
+    {
+        TcpClient client;
+        /*
+         * note: will fail to load form if message not initialised
+         */
+        string message = "hello";
+        public Form1()
+        {
+            InitializeComponent();
+            label1.Text = "Disconnected from server...";
+            button2.Text = "Connect";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Initialise send button
+            button1.Enabled = (textBox2.Text == "") ? false : true;
+
+            // -> show message
+            // Connect
+            //clientSocket.Connect("127.0.0.1", 8888);
+            try
+            {
+                client = new TcpClient("127.0.0.1", 8888);
+                //clientSocket = new TcpClient("196.168.1.6", 8888);
+                label1.Text = "Connected with server...";
+                button2.Text = "Disconnect";
+                Communicate();
+
+            }
+            catch (SocketException ex)
+            {
+                Console.WriteLine($"Socket Exception: {ex.Message}");
+                textBox1.Text = $"Socket Exception: {ex.Message}";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                textBox1.Text = ex.Message;
+            }
+            
+        }
+
+        private void Communicate()
+        {
+            var sb = new StringBuilder();
+
+            // Send to server
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes(message);
+
+            try
+            {
+                NetworkStream stream = client.GetStream();
+                /*
+                 * note: to sanitise stream input/output
+                 * currently server message gets jumbled up
+                 */
+                stream.Write(data, 0, data.Length);
+
+                sb.AppendLine($"{DateTime.Now} Client: {message}");
+
+                // Get server response
+                stream.Read(data, 0, data.Length);
+                string serverMessage = System.Text.Encoding.ASCII.GetString(data);
+
+                sb.AppendLine($"{DateTime.Now} Server: {serverMessage}");
+            }
+            catch (Exception ex)
+            {
+                sb.AppendLine(ex.Message);
+            }
+
+            // Output to screen
+            textBox1.AppendText(sb.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            message = textBox2.Text;
+
+            Communicate();
+            textBox2.Clear();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (client != null && client.Connected)
+            {
+                client.Close();
+                button2.Text = "Connect";
+                label1.Text = "Disconnected from server...";
+            }
+            else
+            {
+                Form1_Load(sender, e);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            button1.Enabled = textBox2.Text != "";
+        }
+    }
+}
---
+++ prototype_2/SocketProgram/SocketServer/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocketServer
+{
+    class Program
+    {
+        private const string hostName = "127.0.0.1";
+        private const int port = 8888;
+
+        static void Main(string[] args)
+        {
+            IPHostEntry host = Dns.GetHostEntry(hostName);
+            
+            // Get IPv4 address back of host machine
+            IPAddress ipAddress = host.AddressList
+                .FirstOrDefault(a => a.AddressFamily == AddressFamily.InterNetwork);
+
+            /*
+             * research notes: alternatively use the following
+             */
+            // set the TcpListener on port 13000
+            //int port = 13000;
+            //TcpListener server = new TcpListener(IPAddress.Any, port);
+
+            // Start listening for client requests
+            //server.Start();
+            try
+            {
+                // Create TcpListener and start listening
+                //var server = new TcpListener(ipAddress, port);
+
+                /*
+                 * research notes: currently not working with ipAddress from AddressList
+                 */
+                IPAddress tmp_address = IPAddress.Parse(hostName);
+                var server = new TcpListener(tmp_address, port);
+
+                server.Start();
+                Console.WriteLine($"Listening on address: {ipAddress}, port: {port}");
+
+                // Buffer
+                byte[] bytes = new byte[1024];
+                string data;
+
+                // Enter listening loop
+                while (true)
+                {
+                    Console.Write("Waiting for a connection... ");
+
+                    // Perform a blocking call to accept requests.
+                    TcpClient client = server.AcceptTcpClient();
+                    Console.WriteLine("Connected!");
+
+                    try
+                    {
+                        /*
+                         * research notes: throws error when client closes connection
+                         */
+                        // Get a stream object for reading and writing
+                        NetworkStream stream = client.GetStream();
+
+                        int i;
+                        i = stream.Read(bytes, 0, bytes.Length);
+
+                        while (i != 0)
+                        {
+
+                            // Translate data bytes to a ASCII string.
+                            data = System.Text.Encoding.ASCII.GetString(bytes);
+                            Console.WriteLine($"Received: {data}");
+
+                            /*
+                             *  TO-DO: process data
+                             */
+                            data = data.ToUpper();
+                            byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+
+                            // Send back a response.
+                            stream.Write(msg, 0, msg.Length);
+                            Console.WriteLine($"Sent: {data}");
+
+                            i = stream.Read(bytes, 0, bytes.Length);
+                        }
+
+                        //stream.Flush(); // needed?
+
+                        // Shutdown and end connection
+                        client.Close();
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.WriteLine("Inside Exception");
+                    }
+                }
+            }
+            catch (SocketException e)
+            {
+                Console.WriteLine($"SocketException: {e}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.WriteLine("Hit enter to continue...");
+            Console.Read();
+        }
+    }
+}
